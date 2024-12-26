c479e071780c5f0eaa342d06df1c224157a77449
Latesh <patillatesh17@gmail.com>
2024-12-26 12:47:21 +0530

    Version 1.2

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-    return "flask application version 1.4"
+    return "flask application version 1.2"
 
 app.run(host="0.0.0.0", port=4000)
 
